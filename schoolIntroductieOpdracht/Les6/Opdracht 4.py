d068736211bced1d155be69a6d5410ea061745a7
AtillaBosma <atillabosma@hotmail.com>
2016-11-03 22:59:04 +0100

    opdracht 4 afgemaakt

--- schoolIntroductieOpdracht/Les6/Opdracht 4.py
+++ schoolIntroductieOpdracht/Les6/Opdracht 4.py
@@ -1,1 +1,6 @@
 #Ik snapte de opdracht niet dus zal ik dit later vragen
+bruin = {"best","beukenlaan","helmond 't hout","helmond","helmond brouwhuis","deurne"}
+groen = {"best","beukenlaan","geldrop","heeze","weert"}
+print("De volgende stations komen in beide trajecten voor:" + bruin.intersection(groen))
+print("Deze stations verschillen met het groene traject:" + bruin.difference(groen))
+print("Dit zijn alle stations:" + groen.union(bruin))
